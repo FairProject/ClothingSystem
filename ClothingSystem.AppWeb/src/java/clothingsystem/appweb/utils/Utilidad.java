09c630ae42e56cff9c5d4682345942cd0d302707
ADONAY <moisesmaba23@gmail.com>
2022-08-01 17:58:44 -0600

    Iniciando los archivos de la Aplicación Web

---
+++ ClothingSystem.AppWeb/src/java/clothingsystem/appweb/utils/Utilidad.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clothingsystem.appweb.utils;
+
+/**
+ *
+ * @author moise
+ */
+public class Utilidad {
+    
+}
