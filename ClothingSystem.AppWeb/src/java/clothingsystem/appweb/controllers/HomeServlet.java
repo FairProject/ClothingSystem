73d802089e1a3adc44fb2018518586df151ebbfd
ADONAY <moisesmaba23@gmail.com>
2022-08-01 18:26:06 -0600

    Se programarón los controlladores: HomeServlet, RolServlet y UsuarioServlet. Se programarón también la interfaz de IAuthorize y las 2 clases de Session y Utilidad

--- ClothingSystem.AppWeb/src/java/clothingsystem/appweb/controllers/HomeServlet.java
+++ ClothingSystem.AppWeb/src/java/clothingsystem/appweb/controllers/HomeServlet.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package clothingsystem.appweb.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,75 +8,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
- * @author moise
+ * En este Servlet, vamos a recibir todas las peticiones get que se realice al
+ * Servlet Home. Aprender conceptos básicos de servlets
+ * http://www.jtech.ua.es/j2ee/2002-2003/modulos/servlets/apuntes/apuntes1_1.htm
+ * Actualizamos la anotación WebServlet para cambiar el atributo urlPatterns
+ * para acceder al Servlet Home utilizando la siguiente Url: la del sitio web
+ * mas /Home
  */
-@WebServlet(name = "HomeServlet", urlPatterns = {"/HomeServlet"})
+@WebServlet(name = "HomeServlet", urlPatterns = {"/Home"})
 public class HomeServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * En este método vamos a direccionar al jsp index que esta la carpeta raiz
+     * de la aplicacion web
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @param request en este parámetro vamos a recibir el request de la
+     * peticion get enviada al servlet Home
+     * @param response en este parámetro vamos a recibir el response de la
+     * peticion get enviada al servlet Home que utlizaremos para enviar el jsp
+     * al navegador web
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // El request.getRequestDispatcher nos permite direccionar a un jsp desde un servlet. 
+        // Enviaremos index.jsp que se encuentra en la carpeta raiz de la aplicacion web
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP <code>GET</code> method.
+     * Este método es un override al método de la clase HttpServlet para recibir
+     * todas las peticiones get que se realice al Servlet Home
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @param request en este parámetro vamos a recibir el request de la
+     * peticion get enviada al servlet Home
+     * @param response en este parámetro vamos a recibir el response de la
+     * peticion get enviada al servlet Home que utlizaremos para enviar el jsp
+     * al navegador web
+     * @throws ServletException devolver una exception de un servlet
+     * @throws IOException devolver una exception al leer o escribir un archivo
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doGetRequestIndex(request, response); // Ir al método doGetRequestIndex
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    // </editor-fold>
 
 }
--- ClothingSystem.AppWeb/src/java/clothingsystem/appweb/utils/IAuthorize.java
+++ ClothingSystem.AppWeb/src/java/clothingsystem/appweb/utils/IAuthorize.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package clothingsystem.appweb.utils;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+
 /**
- *
- * @author moise
+ * Esta interface la utilizaremos para usar una expresión Lambda, al momento de
+ * autorizar un usuario en un servlet. Para conocer mas sobre el tema de
+ * expresion Lambda en java
+ * https://www.adictosaltrabajo.com/2015/12/04/expresiones-lambda-con-java-8/
  */
 public interface IAuthorize {
-    
+
+    void authorize() throws ServletException, IOException;
 }
