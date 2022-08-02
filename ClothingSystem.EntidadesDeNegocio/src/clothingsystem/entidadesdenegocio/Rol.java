69229e24f3a5369aae09d382a60169751d2cf9e9
ADONAY <moisesmaba23@gmail.com>
2022-08-01 18:03:25 -0600

    Actualización de EntidadesDeNegocio

--- ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Rol.java
+++ ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Rol.java
@@ -1,13 +1,61 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package clothingsystem.entidadesdenegocio;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author moise
+ * @author Dev3hc01
  */
 public class Rol {
+
+    private int id;
+    private String nombre;
+    private int top_aux;
+    private ArrayList<Usuario> usuarios;
+
+    public Rol() {
+    }
+
+    public Rol(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public ArrayList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(ArrayList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
     
 }
