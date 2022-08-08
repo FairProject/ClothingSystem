b7c411cbb9257cfb35a1ea7c602488bba6ec7bf8
HP <HP@DESKTOP-OOE8CFG>
2022-08-08 08:47:29 -0500

    programar la clase de ropafoto de entidad de negocio

--- ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/RopaFoto.java
+++ ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/RopaFoto.java
@@ -9,5 +9,61 @@ package clothingsystem.entidadesdenegocio;
  * @author HP
  */
 public class RopaFoto {
-    
+
+    private int id;
+    private int idRopa;
+    private String url;
+    private byte estatus;
+    private int top_aux;
+
+    public RopaFoto() {
+    }
+
+    public RopaFoto(int id, int idRopa, String url, byte estatus) {
+        this.id = id;
+        this.idRopa = idRopa;
+        this.url = url;
+        this.estatus = estatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdRopa() {
+        return idRopa;
+    }
+
+    public void setIdRopa(int idRopa) {
+        this.idRopa = idRopa;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
 }
