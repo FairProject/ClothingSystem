a8a35e2b70e6ded925cfb9c801ae2adc347df07a
HP <HP@DESKTOP-OOE8CFG>
2022-08-11 17:07:06 -0500

    solucionando errores

--- ClothingSystem.AccesoADatos/src/clothingsystem/accesoadatos/ComunDB.java
+++ ClothingSystem.AccesoADatos/src/clothingsystem/accesoadatos/ComunDB.java
@@ -19,15 +19,20 @@ public class ComunDB {
             + "password=12345;"
             + "loginTimeout=30;encrypt=false;trustServerCertificate=false";*/
     
-     static String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS02:1433;"
-            + "database=ClothingSystemdb;"
-            + "user=Alexander;"
-            + "password=28102002;"
-            + "loginTimeout=30;encrypt=false;trustServerCertificate=false";
+//     static String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS02:1433;"
+//            + "database=ClothingSystemdb;"
+//            + "user=Alexander;"
+//            + "password=28102002;"
+//            + "loginTimeout=30;encrypt=false;trustServerCertificate=false";
     // Documentacion para entender por que se esta utilizando throws SQLException
     // en los siguientes metodos --> http://dis.um.es/~bmoros/Tutorial/parte9/cap9-3.html
     
     
+     static String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS02:1433;"
+            + "database=ClothingSystemdb;"
+            + "user=java2;"
+            + "password=12345678;"
+            + "loginTimeout=30;encrypt=false;trustServerCertificate=false";
     /* El metodo obtenerConexion() nos va a devolver una conexion abierta al gestor de base de datos que 
      estemos utilizando*/
     public static Connection obtenerConexion() throws SQLException {
