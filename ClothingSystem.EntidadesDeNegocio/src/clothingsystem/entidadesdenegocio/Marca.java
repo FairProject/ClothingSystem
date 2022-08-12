a2e23ca12c8def3faa64a259ae55d51ab6e9daaf
Edwin Alexander Bernardino Morán <814892@clases.edu.sv>
2022-08-12 10:00:08 -0600

    Terminando de programar entidades de negocio de el archivo marca

--- ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Marca.java
+++ ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Marca.java
@@ -9,5 +9,72 @@ package clothingsystem.entidadesdenegocio;
  * @author edwin
  */
 public class Marca {
+    private int id;
+    private String nombre;
+    private byte estatus;
+    private String descripcion;
+    private String paisOrigen;
+    private int top_aux;
+
+    public Marca() {
+        
+    }
+
+    public Marca(int id, String nombre, byte estatus, String descripcion, String paisOrigen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.estatus = estatus;
+        this.descripcion = descripcion;
+        this.paisOrigen = paisOrigen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPaisOrigen() {
+        return paisOrigen;
+    }
+
+    public void setPaisOrigen(String paisOrigen) {
+        this.paisOrigen = paisOrigen;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+    
     
 }
