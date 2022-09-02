2f5c932ae38caee58ec159be7bdbc8d46675c7cb
Moisés Bonilla <86023579+ADONAY1234@users.noreply.github.com>
2022-09-01 21:04:33 -0600

    Se modificó AccesoADatos

--- ClothingSystem.AccesoADatos/src/clothingsystem/accesoadatos/RopaDAL.java
+++ ClothingSystem.AccesoADatos/src/clothingsystem/accesoadatos/RopaDAL.java
@@ -383,9 +383,9 @@ public class RopaDAL { // Clase para poder realizar consulta de Insertar, modifi
             }
             sql += obtenerCampos(); // Obtener los campos de la tabla de Usuario que iran en el SELECT
             sql += ",";
-            sql += RolDAL.obtenerCampos(); // Obtener los campos de la tabla de Rol que iran en el SELECT
+            sql += MarcaDAL.obtenerCampos(); // Obtener los campos de la tabla de Rol que iran en el SELECT
             sql += " FROM Ropa r";
-            sql += " JOIN Marca m on (r.IdMarca=r.Id)"; // agregar el join para unir la tabla de Usuario con Rol
+            sql += " JOIN Marca m on (m.IdMarca=r.Id)"; // agregar el join para unir la tabla de Usuario con Rol
             ComunDB comundb = new ComunDB();
             ComunDB.UtilQuery utilQuery = comundb.new UtilQuery(sql, null, 0);
             querySelect(pRopa, utilQuery); // Asignar el filtro a la consulta SELECT de la tabla de Usuario 
