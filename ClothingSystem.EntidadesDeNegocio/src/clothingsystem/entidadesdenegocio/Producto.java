1020ebfa7d2dbe87d544f22879ea0ffc7d9f2dc3
carlos77 <carloscolon5479garcia@outlook.es>
2022-12-02 18:03:04 -0600

    agregar la foto a producto

--- ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Producto.java
+++ ClothingSystem.EntidadesDeNegocio/src/clothingsystem/entidadesdenegocio/Producto.java
@@ -2,6 +2,7 @@ package clothingsystem.entidadesdenegocio;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 /**
  *
  * @author Cristopher
@@ -19,14 +20,14 @@ public class Producto {
     private LocalDate fechaVencimiento;
     private LocalDateTime fechaCreacion;
     private String comentario;
+    private String foto;
     private int top_aux;
     private Categoria categoria;
-    //private Marca marca;
 
     public Producto() {
     }
 
-    public Producto(int id, int idCategoria, String codigo, String descripcion, double precioCompra, double precioVenta, int Existencia, byte estatus, LocalDate fechaVencimiento, LocalDateTime fechaCreacion, String comentario) {
+    public Producto(int id, int idCategoria, String codigo, String descripcion, double precioCompra, double precioVenta, int Existencia, byte estatus, LocalDate fechaVencimiento, LocalDateTime fechaCreacion, String comentario, String foto) {
         this.id = id;
         this.idCategoria = idCategoria;
         this.codigo = codigo;
@@ -38,191 +39,123 @@ public class Producto {
         this.fechaVencimiento = fechaVencimiento;
         this.fechaCreacion = fechaCreacion;
         this.comentario = comentario;
+        this.foto = foto;
     }
 
-    /**
-     * @return the id
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the idCategoria
-     */
     public int getIdCategoria() {
         return idCategoria;
     }
 
-    /**
-     * @param idCategoria the idCategoria to set
-     */
     public void setIdCategoria(int idCategoria) {
         this.idCategoria = idCategoria;
     }
 
-    /**
-     * @return the codigo
-     */
     public String getCodigo() {
         return codigo;
     }
 
-    /**
-     * @param codigo the codigo to set
-     */
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
-    /**
-     * @return the descripcion
-     */
     public String getDescripcion() {
         return descripcion;
     }
 
-    /**
-     * @param descripcion the descripcion to set
-     */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
-    /**
-     * @return the precioCompra
-     */
     public double getPrecioCompra() {
         return precioCompra;
     }
 
-    /**
-     * @param precioCompra the precioCompra to set
-     */
     public void setPrecioCompra(double precioCompra) {
         this.precioCompra = precioCompra;
     }
 
-    /**
-     * @return the precioVenta
-     */
     public double getPrecioVenta() {
         return precioVenta;
     }
 
-    /**
-     * @param precioVenta the precioVenta to set
-     */
     public void setPrecioVenta(double precioVenta) {
         this.precioVenta = precioVenta;
     }
 
-    /**
-     * @return the Existencia
-     */
     public int getExistencia() {
         return Existencia;
     }
 
-    /**
-     * @param Existencia the Existencia to set
-     */
     public void setExistencia(int Existencia) {
         this.Existencia = Existencia;
     }
 
-    /**
-     * @return the estatus
-     */
     public byte getEstatus() {
         return estatus;
     }
 
-    /**
-     * @param estatus the estatus to set
-     */
     public void setEstatus(byte estatus) {
         this.estatus = estatus;
     }
 
-    /**
-     * @return the fechaVencimiento
-     */
     public LocalDate getFechaVencimiento() {
         return fechaVencimiento;
     }
 
-    /**
-     * @param fechaVencimiento the fechaVencimiento to set
-     */
     public void setFechaVencimiento(LocalDate fechaVencimiento) {
         this.fechaVencimiento = fechaVencimiento;
     }
 
-    /**
-     * @return the fechaCreacion
-     */
     public LocalDateTime getFechaCreacion() {
         return fechaCreacion;
     }
 
-    /**
-     * @param fechaCreacion the fechaCReacion to set
-     */
     public void setFechaCreacion(LocalDateTime fechaCreacion) {
         this.fechaCreacion = fechaCreacion;
     }
 
-    /**
-     * @return the comentario
-     */
     public String getComentario() {
         return comentario;
     }
 
-    /**
-     * @param comentario the comentario to set
-     */
     public void setComentario(String comentario) {
         this.comentario = comentario;
     }
 
-    /**
-     * @return the top_aux
-     */
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
     public int getTop_aux() {
         return top_aux;
     }
 
-    /**
-     * @param top_aux the top_aux to set
-     */
     public void setTop_aux(int top_aux) {
         this.top_aux = top_aux;
     }
 
-    /**
-     * @return the categoría
-     */
     public Categoria getCategoria() {
         return categoria;
     }
 
-    /**
-     * @param categoria the categoría to set
-     */
-    public void setCategoría(Categoria categoria) {
+    public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
     
-     public class EstatusProducto {
+
+    public class EstatusProducto {
 
         public static final byte ENEXISTENCIA = 1;
         public static final byte NOHAYEXISTENCIAS = 2;
